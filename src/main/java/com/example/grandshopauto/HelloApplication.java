4aff1658073fbce99a782654a99ee58de4c72e20
Abdoll <96470320+YouGotTrolled@users.noreply.github.com>
2025-01-04 00:59:21 +0330

    size
    031012

--- src/main/java/com/example/grandshopauto/HelloApplication.java
+++ src/main/java/com/example/grandshopauto/HelloApplication.java
@@ -11,11 +11,15 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         Image ic = new Image("u.png");
+        stage.setMinHeight(720);
+        stage.setMinWidth(1280);
         stage.getIcons().add(ic);
         stage.setTitle("Grand Shop Auto");
+        //scene.getStylesheets().add(getClass().getResource("app.css").toExternalForm());
         stage.setScene(scene);
+
         stage.show();
     }
 
