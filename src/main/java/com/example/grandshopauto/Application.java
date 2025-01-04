2f582ba005677053fb5ca4c3d06573f91aa7360b
Abdoll <96470320+YouGotTrolled@users.noreply.github.com>
2025-01-04 03:51:22 +0330

    Update Application.java

--- src/main/java/com/example/grandshopauto/Application.java
+++ src/main/java/com/example/grandshopauto/Application.java
@@ -10,14 +10,14 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader root1 = new FXMLLoader(Application.class.getResource("root1.fxml"));
-        Scene scene = new Scene(root1.load(), 1280, 720);
+        Scene scene1 = new Scene(root1.load(), 1280, 720);
         Image ic = new Image("u.png");
         stage.setMinHeight(720);
         stage.setMinWidth(1280);
         stage.getIcons().add(ic);
         stage.setTitle("Grand Shop Auto");
         //scene.getStylesheets().add(getClass().getResource("app.css").toExternalForm());
-        stage.setScene(scene);
+        stage.setScene(scene1);
 
         stage.show();
     }
