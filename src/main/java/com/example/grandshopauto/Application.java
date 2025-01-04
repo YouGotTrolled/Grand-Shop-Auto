cd3d5c9e50a066992dc809adc38e41a84d2e753b
Abdoll <96470320+YouGotTrolled@users.noreply.github.com>
2025-01-04 05:01:37 +0330

    css

--- src/main/java/com/example/grandshopauto/Application.java
+++ src/main/java/com/example/grandshopauto/Application.java
@@ -15,6 +15,7 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader root1 = new FXMLLoader(Application.class.getResource("root1.fxml"));
         Scene scene1 = new Scene(root1.load(), 1280, 720);
+        scene1.getStylesheets().add(getClass().getResource("app1.css").toExternalForm());
         Image ic = new Image("u.png");
         stage.setMinHeight(720);
         stage.setMinWidth(1280);
@@ -24,9 +25,8 @@ public class Application extends javafx.application.Application {
         stage.show();
         FXMLLoader root2 = new FXMLLoader(Application.class.getResource("root2.fxml"));
         Scene scene2 = new Scene(root2.load(), 1280, 720);
-        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(3000), event -> {
-            stage.setScene(scene2);
-        }));
+        scene2.getStylesheets().add(getClass().getResource("app2.css").toExternalForm());
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(3000), event -> {stage.setScene(scene2);}));
         timeline.play();
     }
 
