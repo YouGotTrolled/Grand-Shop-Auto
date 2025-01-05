3f7100f231ba345560805b3b3b7cb9de2ce4ea6b
MahdiAlinejhad84 <ali.iran.84.2024@gmail.com>
2025-01-05 08:13:48 +0330

    g

---
+++ src/main/java/com/example/grandshopauto/Controller2.java
@@ -0,0 +1,14 @@
+package com.example.grandshopauto;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class Controller2 {
+    @FXML
+    private ListView<String> border;
+    @FXML
+    public void initialize() {
+        border.getItems().addAll("Item 1", "Item 2", "Item 3");
+    }
+}
