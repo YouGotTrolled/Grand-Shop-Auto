4d260cb1d96bb46ba73eafbe3692eab429e047db
Abdoll <96470320+YouGotTrolled@users.noreply.github.com>
2025-01-06 23:53:19 +0330

    loging

--- src/main/java/com/example/grandshopauto/Application.java
+++ src/main/java/com/example/grandshopauto/Application.java
@@ -7,8 +7,14 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
 
 public class Application extends javafx.application.Application {
+    PrintWriter loger;
     @Override
     public void start(Stage stage) {
         try {
@@ -24,6 +30,8 @@ public class Application extends javafx.application.Application {
             Scene scene3 = new Scene(root2.load(), 1280, 720);
             scene3.getStylesheets().add(getClass().getResource("app2.css").toExternalForm());
 
+            loger = new PrintWriter(new BufferedOutputStream(new FileOutputStream("log.txt",true)));
+
             Image ic = new Image("u.png");
             stage.setMinHeight(720);
             stage.setMinWidth(1280);
@@ -35,9 +43,14 @@ public class Application extends javafx.application.Application {
                 stage.setScene(scene2);
             }));
             timeline.play();
-        } catch (Exception e) {
+            loger.close();
+        }catch(FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("error in file finding");
+        }catch(Exception e) {
             e.printStackTrace();
             System.out.println("error");
+            loger.println("("+LocalDateTime.now()+"):"+e.getMessage());
         }
     }
 
