685b83ea9dd50a11c6cd0334208fc373274d71e6
Abdoll <96470320+YouGotTrolled@users.noreply.github.com>
2025-01-06 23:16:27 +0330

    tt

--- src/main/java/com/example/grandshopauto/Application.java
+++ src/main/java/com/example/grandshopauto/Application.java
@@ -8,31 +8,37 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import java.io.IOException;
-
 public class Application extends javafx.application.Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader root1 = new FXMLLoader(Application.class.getResource("root1.fxml"));
-        Scene scene1 = new Scene(root1.load(), 1280, 720);
-        scene1.getStylesheets().add(getClass().getResource("app1.css").toExternalForm());
+    public void start(Stage stage) {
+        try {
+            FXMLLoader root1 = new FXMLLoader(Application.class.getResource("root1.fxml"));
+            Scene scene1 = new Scene(root1.load(), 1280, 720);
+            scene1.getStylesheets().add(getClass().getResource("app1.css").toExternalForm());
 
-        FXMLLoader login = new FXMLLoader(Application.class.getResource("loginJ.fxml"));
-        Scene scene2 = new Scene(login.load(), 1280, 720);
-        scene2.getStylesheets().add(getClass().getResource("loginCSS.css").toExternalForm());
+            FXMLLoader login = new FXMLLoader(Application.class.getResource("loginJ.fxml"));
+            Scene scene2 = new Scene(login.load(), 1280, 720);
+            scene2.getStylesheets().add(getClass().getResource("loginCSS.css").toExternalForm());
 
+            FXMLLoader root2 = new FXMLLoader(Application.class.getResource("loginJ.fxml"));
+            Scene scene3 = new Scene(root2.load(), 1280, 720);
+            scene3.getStylesheets().add(getClass().getResource("app2.css").toExternalForm());
 
-
-        Image ic = new Image("u.png");
-        stage.setMinHeight(720);
-        stage.setMinWidth(1280);
-        stage.getIcons().add(ic);
-        stage.setTitle("Grand Shop Auto");
-        stage.setScene(scene1);
-        stage.show();
-        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(4000), event -> {stage.setScene(scene2);}));
-        timeline.play();
-
+            Image ic = new Image("u.png");
+            stage.setMinHeight(720);
+            stage.setMinWidth(1280);
+            stage.getIcons().add(ic);
+            stage.setTitle("Grand Shop Auto");
+            stage.setScene(scene1);
+            stage.show();
+            Timeline timeline = new Timeline(new KeyFrame(Duration.millis(4000), event -> {
+                stage.setScene(scene2);
+            }));
+            timeline.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("error");
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/grandshopauto/loginC.java
+++ src/main/java/com/example/grandshopauto/loginC.java
@@ -1,40 +1,11 @@
-//package com.example.grandshopauto;
-//
-//
-//import javafx.fxml.FXML;
-//import javafx.scene.control.Button;
-//import javafx.scene.control.TextField;
-//import java.util.Scanner;
-//
-//
-//public class loginC {
-//
-//    @FXML
-//    private Button login;
-//
-//    @FXML
-//    private void onLogin() {
-//
-//    }
-//
-//    @FXML
-//    private  TextField nameKarbari;
-//
-//    @FXML
-//    public void initialize() {
-//        Scanner scan = new Scanner(System.in);
-//        nameKarbari.setText(scan.nextLine());
-//
-//    }
-//}
 package com.example.grandshopauto;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -44,16 +15,16 @@ public class loginC {
     @FXML
     public void signUp(ActionEvent actionEvent) {
         try {
-            FXMLLoader root2 = new FXMLLoader(Application.class.getResource("loginJ.fxml"));
-            Scene scene3 = new Scene(root2.load(), 1280, 720);
-            scene3.getStylesheets().add(getClass().getResource("app2.css").toExternalForm());
-
+            Parent root2 = FXMLLoader.load(getClass().getResource("root2.fxml"));
+            Scene scene=new Scene(root2,1280,720);
+            scene.getStylesheets().add(getClass().getResource("app2.css").toExternalForm());
             Stage sign = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-            sign.setScene(scene3);
+            sign.setScene(scene);
             sign.show();
         }
         catch (IOException e) {
             e.printStackTrace();
+            System.out.println("ll");
         }
     }
 }
