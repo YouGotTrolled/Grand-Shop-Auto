c8ab7f656f615ac021a50d3a74f01d567418a835
Abdoll <96470320+YouGotTrolled@users.noreply.github.com>
2025-01-08 00:44:11 +0330

    mehdi bjon

--- src/main/java/com/example/grandshopauto/SignC.java
+++ src/main/java/com/example/grandshopauto/SignC.java
@@ -9,10 +9,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
 import java.io.*;
 import java.time.LocalDateTime;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class SignC {
@@ -47,63 +45,106 @@ public class SignC {
     @FXML
     public void sub(ActionEvent actionEvent) {
         try {
-            boolean flag = true;
-            boolean flagg = true;
-            boolean flaggg = true;
-            boolean flagggg = true;
+            boolean flag=true;
+            boolean flagPas1 = true;
+            boolean flagPas2 = true;
+            boolean flagPas3 = true;
+            boolean flagPas4 = true;
             loger = new PrintWriter(new BufferedOutputStream(new FileOutputStream("log.txt",true)));
             checker=new BufferedReader(new FileReader("userInfo.txt"));
-            String tuser=user.getText();
-            String ttuser;
-            String pass = pas.getText();
             String bob="نام کاربری تکراری است";
-            if(checker.readLine()==null) {
-                flag=false;
+            for(int i=0;(flag)&&i<user.getText().length();i++){
+                if(user.getText().isEmpty()||(int)user.getText().charAt(i)>122||((int)user.getText().charAt(i)>90&&(int)user.getText().charAt(i)<97)||((int)user.getText().charAt(i)>57&&(int)user.getText().charAt(i)<65)||(int)user.getText().charAt(i)<48){
+                    flag=false;
+                    bob="یوزرنیم فقط میتواند عدد و حروف انگلیسی باشد";
+                }
             }
-            while(flag&&checker.readLine()!=null) {
-                spt = new StringTokenizer(checker.readLine(), ",[]");
-                ttuser = spt.nextToken();
-                if (!tuser.equals(ttuser)) {
+            if(flag) {
+                while (flag && checker.read()!=-1) {
+                    spt = new StringTokenizer(checker.readLine(), ",[]");
+                    if (user.getText().equals(spt.nextToken())) {
+                        flag= false;
+                    }
+                }
+            }
+            if(flag){
+                flagPas1=!(charCheck(pas.getText(),48,57));
+                if (flagPas1){
+                    bob="رمز عدد ندارد";
+                }
+                flagPas2=!(charCheck(pas.getText(),97,122));
+                if (flagPas2){
+                    bob = "رمز حرف کوچک انگلیسی ندارد";
+                }
+                flagPas3=!(charCheck(pas.getText(),65,90));
+                if (flagPas3){
+                    bob="رمز حرف بزرگ انگلیسی ندارد";
+                }
+                flagPas4=!(charCheck(pas.getText(),33,38));
+                if (flagPas4){
+                    bob="رمز کاراکتر خاص ندارد ندارد";
+                }
+                if (flagPas4||flagPas3||flagPas2||flagPas1){
+                    flag=false;
+                }
+            }
+            if(flag){
+                for (int i = 0; (flag) && i < pas.getText().length(); i++) {
+                    if ((int) pas.getText().charAt(i) > 122 || ((int) pas.getText().charAt(i) > 90 && (int) pas.getText().charAt(i) < 97) || ((int) pas.getText().charAt(i) > 57 && (int) pas.getText().charAt(i) < 65) || ((int) pas.getText().charAt(i) > 38 && (int) pas.getText().charAt(i) < 48)|| (int) pas.getText().charAt(i) < 33) {
+                        flag = false;
+                        bob = "رمز فقط میتواند عدد و حروف انگلیسی و حروف خاص باشد";
+                    }
+                }
+            }
+            if(flag) {
+                if (!(pas.getText().equals(Tpas.getText()))) {
+                    bob = "رمز ها یکی نیستن";
                     flag = false;
                 }
             }
-            if(flagg&&!flag){
-                for(int i=0;i<pass.length();i++){
-                    if((int) pass.charAt(i) >57&&(int)pass.charAt(i)<48){
-                        bob="رمز عدد ندارد";
-                        flagg=false;
-                    }
-                    if((int) pass.charAt(i) >122&&(int)pass.charAt(i)<97){
-                        bob = "رمز حرف کوچک ندارد";
-                        flagg=false;
-                    }
-                    if((int) pass.charAt(i) >90&&(int)pass.charAt(i)<65){
-                        bob="رمز حرف بزرگ ندارد";
-                        flagg=false;
-                    }
-                    if((int) pass.charAt(i) >38&&(int)pass.charAt(i)<33){
-                        bob="رمز کاراکتر خاص ندارد ندارد";
-                        flagg=false;
-                    }
-                    if(!flagg){
-                        flagg=true;
-                    }
-                    else{
-                        flagg=false;
-                    }
-
+            if(flag){
+                flag=!((charCheckOut(name.getText(),1740,1570)&&!charCheck(name.getText(),32,32))||name.getText().isEmpty());
+                if(!flag){
+                    bob="نام فقط باید حروف فارسی باشد";
                 }
             }
-            if(flaggg&&!flagg){
-                if(!(pass.equals(Tpas.getText()))){
-                    bob="رمز ها یکی نیستن";
+            if(flag){
+                flag=!((charCheckOut(Lname.getText(),1740,1570)&&!charCheck(Lname.getText(),32,32))||Lname.getText().isEmpty());
+                if(!flag){
+                    bob="نام خوانوادگی فقط باید حروف فارسی باشد";
                 }
-                else {
-                    flagggg=false;
+            }
+            if(flag){
+                flag=!(charCheckOut(ID.getText(),57,48)||ID.getText().isEmpty());
+                if(!flag){
+                    bob="کد ملی فقط باید عدد باشد";
+                }
+            }
+            if(flag){
+                flag=!(((charCheckOut(Lname.getText(),57,48)&&!charCheck(Lname.getText(),47,47))) || DOB.getText().isEmpty()) || DOB.getText().charAt(5)!='/'|| DOB.getText().charAt(8)!='/';
+                if(!flag){
+                    bob="تاریخ تولد فقط باید عدد و / باشد";
+                }
+            }
+            if(flag){
+                flag=!(charCheckOut(num.getText(),57,48)||num.getText().isEmpty());
+                if(!flag){
+                    bob="شماره فقط باید عدد باشد";
+                }
+            }
+            if(flag){
+                if(ADD.getText().isEmpty()){
+                    flag=false;
+                }
+                for(int i=0;flag&&i<ADD.getText().length();i++) {
+                    if (ADD.getText().charAt(i) == '[' || ADD.getText().charAt(i) == ']' || ADD.getText().charAt(i) == ',') {
+                        flag = false;
+                        bob = "از کاراکتر های غیر مجاز در ادرس استفاده نکنید یا ادرس داده نشد";
+                    }
                 }
             }
             label.setText(bob);
-            if(!flagggg){
+            if(flag){
                 checker.close();
                 adder=new PrintWriter(new BufferedOutputStream(new FileOutputStream("userInfo.txt",true)));
                 adder.println("["+user.getText()+","+pas.getText()+","+ID.getText()+","+DOB.getText()+","+ADD.getText()+","+num.getText()+","+name.getText()+","+Lname.getText()+"]");
@@ -116,7 +157,7 @@ public class SignC {
                 sign1.setScene(scene);
                 sign1.show();
             }
-            loger.println("("+LocalDateTime.now()+"):"+tuser+":\""+bob+"\"");
+            loger.println("("+LocalDateTime.now()+"):"+user.getText()+":\""+bob+"\"");
             loger.close();
         }catch(FileNotFoundException e) {
             e.printStackTrace();
@@ -128,4 +169,23 @@ public class SignC {
             loger.println("("+ LocalDateTime.now()+"):\""+e.getMessage()+"\"in SingC.java");
         }
     }
+    private boolean charCheck (String a,int b,int c){
+        boolean an=false;
+        for(int i=0;(!an)&&i<a.length();i++){
+            if((int)a.charAt(i)>=b&&(int)a.charAt(i)<=c){
+                an=true;
+            }
+        }
+        return an;
+    }
+    private boolean charCheckOut (String a,int b,int c){
+        boolean an=false;
+        for(int i=0;(!an)&&i<a.length();i++){
+            if((int)a.charAt(i)>b||(int)a.charAt(i)<c){
+                an=true;
+            }
+        }
+        return an;
+    }
+
 }
