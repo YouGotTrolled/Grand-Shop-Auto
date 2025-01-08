e5ce0ecf2000457ca4181430708e54f5473303bb
Abdoll <96470320+YouGotTrolled@users.noreply.github.com>
2025-01-09 02:40:19 +0330

    tt

--- src/main/java/com/example/grandshopauto/Controller2.java
+++ src/main/java/com/example/grandshopauto/Controller2.java
@@ -1,13 +1,28 @@
 package com.example.grandshopauto;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 
 public class Controller2 {
     @FXML
     private ListView<String> border;
     @FXML
+    private Button info;
+    @FXML
     public void initialize() {
-        border.getItems().addAll("Item 1", "Item 2", "Item 3");
+        try {
+            border.getItems().addAll("Item 1", "Item 2", "Item 3");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("error in controler2");
+        }
+    }
+    @FXML
+    public void infoGo(ActionEvent event){
+
     }
 }
