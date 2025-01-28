9d8b98678be548c8e0e00e0f24391b4ed9462e2b
Abdoll <96470320+YouGotTrolled@users.noreply.github.com>
2025-01-28 12:54:23 +0330

    alldone

--- src/main/java/com/example/grandshopauto/adminLog.java
+++ src/main/java/com/example/grandshopauto/adminLog.java
@@ -2,8 +2,15 @@ package com.example.grandshopauto;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.*;
 
 public class adminLog {
 
@@ -14,13 +21,49 @@ public class adminLog {
     private TextArea messageArea;
 
     @FXML
-    void back(ActionEvent event) {
+    private void initialize() {
+        messageArea.appendText("\"all\" for log.txt\nusername for username\\pLog.txt\n");
+        loadUser.setOnAction(e -> loadUserButton(e));
+    }
 
+    @FXML
+    void back(ActionEvent event) throws IOException {
+        Parent back1 = FXMLLoader.load(getClass().getResource("infoAdmin.fxml"));
+        Scene scene = new Scene(back1, 1280 ,720);
+        Stage back2 = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        back2.setScene(scene);
+        back2.show();
     }
 
     @FXML
     void loadUserButton(ActionEvent event) {
+        File userF = new File(".\\userInfo\\" + loadUser.getText());
+        messageArea.clear();
+        if(loadUser.getText().equals("all")){
+            load(".\\systemFiles\\log.txt");
+        }else if(userF.exists()){
+            load(".\\userInfo\\" + loadUser.getText() + "\\pLog.txt");
+        }else{
+            messageArea.appendText("wrong input\n");
+        }
+    }
 
+    private void load(String logLoc){
+        try {
+            String temp;
+            messageArea.clear();
+            BufferedReader reader = new BufferedReader(new FileReader(logLoc));
+            temp = reader.readLine();
+            while (temp != null) {
+                messageArea.appendText(temp + "\n");
+                temp = reader.readLine();
+            }
+            reader.close();
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/main/java/com/example/grandshopauto/infoAdminC.java
+++ src/main/java/com/example/grandshopauto/infoAdminC.java
@@ -7,15 +7,72 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class infoAdminC {
 
     @FXML
-    private void initialize() {
+    private Label DOB;
 
+    @FXML
+    private Label ID;
+
+    @FXML
+    private Label addr;
+
+    @FXML
+    private Label lastName;
+
+    @FXML
+    private Label name;
+
+    @FXML
+    private Label num;
+
+    @FXML
+    private void initialize() {
+        try {
+            String temp;
+            String user;
+            BufferedReader system = new BufferedReader(new FileReader(".\\systemFiles\\cUser.txt"));
+            StringTokenizer spt = new StringTokenizer(system.readLine(), ":");
+            temp = spt.nextToken();
+            user = spt.nextToken();
+            system.close();
+            BufferedReader checker = new BufferedReader(new FileReader(".\\userInfo\\" + user + "\\acInfo.txt"));
+            temp = checker.readLine();
+            temp = checker.readLine();
+            spt = new StringTokenizer(checker.readLine(), ":");
+            temp = spt.nextToken();
+            ID.setText(spt.nextToken());
+            spt = new StringTokenizer(checker.readLine(), ":");
+            temp = spt.nextToken();
+            DOB.setText(spt.nextToken());
+            spt = new StringTokenizer(checker.readLine(), ":");
+            temp = spt.nextToken();
+            addr.setText(spt.nextToken());
+            spt = new StringTokenizer(checker.readLine(), ":");
+            temp = spt.nextToken();
+            num.setText(spt.nextToken());
+            spt = new StringTokenizer(checker.readLine(), ":");
+            temp = spt.nextToken();
+            name.setText(spt.nextToken());
+            spt = new StringTokenizer(checker.readLine(), ":");
+            temp = spt.nextToken();
+            lastName.setText(spt.nextToken());
+            checker.close();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
     @FXML
     private void back(ActionEvent event) throws IOException {
